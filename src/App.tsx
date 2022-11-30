63246dcfc3e14baecfb8dc0380d936074494550c
taigakiyokawa <kiyokawa.taiga@moneyforward.co.jp>
2022-11-30 23:14:29 +0900

    [feat] Add confetti with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FC } from "react";
+import { useReward } from "react-rewards";
 
-function App() {
+export const App: FC = () => {
+  const { reward, isAnimating } = useReward("rewardId", "confetti");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button disabled={isAnimating} onClick={reward} className="button">
+        <span id="rewardId" />
+        🎉
+      </button>
     </div>
   );
-}
-
-export default App;
+};
